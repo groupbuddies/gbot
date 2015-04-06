fa0ad1a8c32ea474ff1b9ddbce1dbbf814bc38fd
Miguel Palhas <mpalhas@gmail.com>
2015-04-06 16:59:22 +0100

    Attempting to create a pull requests script

---
+++ scripts/github.js
@@ -0,0 +1,21 @@
+require('dotenv').load();
+var Headquarters = require('headquarters-node');
+
+var hq = Headquarters({
+  clientID: process.env.HQ_APP_ID,
+  clientSecret: process.env.HQ_APP_SECRET,
+  callbackURL: 'https://gbot.groupbuddies.com/users/auth/headquarters/callback'
+});
+
+module.exports = function(robot) {
+  robot.respond(/prs/i, function(response) {
+    hq.github.pullRequests('is:open').then(function(data) {
+      var prs = data.items.map(function(item) {
+        return "<" + item.url + "|" + item.title + ">";
+      });
+      response.send(JSON.stringify({
+        text: prs.join("\n")
+      }));
+    });
+  });
+}
