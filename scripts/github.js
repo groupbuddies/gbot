a291b8895c7ad3ac411ab520b95ef5fbe49de5c2
Gabriel Poça <gabrielpoca@gmail.com>
2015-04-14 17:31:50 +0100

    Add command to return today's sushi grill images

--- scripts/github.js
+++ scripts/github.js
@@ -22,4 +22,4 @@ module.exports = function(robot) {
       response.send(prs.join("\n"));
     });
   });
-}
+};
---
+++ scripts/lunch.js
@@ -0,0 +1,48 @@
+require('dotenv').load();
+
+var FB = require('fb');
+
+var toDateString = function(date) {
+  return new Date(date).toDateString();
+};
+
+var requestTodayFeed = function(callback) {
+  FB.api('622844924478142/posts', { fields: ['full_picture', 'created_time'] }, function(res) {
+    if (!res || res.error)
+      return;
+
+    var currentDate = new Date();
+    var images = [];
+
+    res.data.forEach(function(post) {
+      if (currentDate.toDateString() == toDateString(post.created_time))
+        images.push(post.full_picture);
+    });
+
+    callback(images.join(' '));
+  });
+};
+
+var requestAccessToken = function(callback) {
+  FB.api('oauth/access_token', {
+    client_id: process.env.FB_APP_ID,
+    client_secret: process.env.FB_APP_SECRET,
+    grant_type: 'client_credentials'
+  }, function(res) {
+    if (!res || res.error)
+      return;
+
+    FB.setAccessToken(res.access_token);
+    callback();
+  });
+};
+
+module.exports = function(robot) {
+  robot.respond(/lunch/i, function(response) {
+    requestAccessToken(function() {
+      requestTodayFeed(function(message) {
+        response.send(message);
+      });
+    });
+  });
+};
