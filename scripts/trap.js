7fd307b5635585452b370a4b620d73145488c9b6
Miguel Palhas <mpalhas@gmail.com>
2015-10-27 11:36:44 +0000

    Better trap message

--- scripts/trap.js
+++ scripts/trap.js
@@ -6,10 +6,10 @@ var hq = require('../lib/headquarters');
 
 module.exports = function(robot) {
   robot.respond(/trap ?(.*)?/i, function(msg) {
-    to = msg.match[1] || "It's a trap!!"
+    to = msg.match[1] || '';
     hq.member.all().then(function(members) {
       var index = Math.floor(Math.random() * members.length);
-      msg.send(members[index].name + ", " + to);
+      msg.send(members[index].name + ", It's a trap!! " + to);
     });
   });
 };
