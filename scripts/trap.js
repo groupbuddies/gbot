7add129880c5c69dcefbff23b2003bfcb1fd82d7
Miguel Palhas <mpalhas@gmail.com>
2015-10-27 11:32:25 +0000

    Adds gbot trap command

---
+++ scripts/trap.js
@@ -0,0 +1,15 @@
+// Commands:
+//    hubot trap - Choose a random Subvisual member
+//    hubot trap [action] - Assign a trap to a random Subvisual member
+
+var hq = require('../lib/headquarters');
+
+module.exports = function(robot) {
+  robot.respond(/trap ?(.*)?/i, function(msg) {
+    to = msg.match[1] || "It's a trap!!"
+    hq.member.all().then(function(members) {
+      var index = Math.floor(Math.random() * members.length);
+      msg.send(members[index].name + ", " + to);
+    });
+  });
+};
